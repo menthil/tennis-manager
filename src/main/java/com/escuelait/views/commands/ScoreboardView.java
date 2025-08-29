d24a74653b4f71906f470402e72eee484314fbca
Héctor Garcia <hgarciaperis@gmail.com>
2025-08-30 00:30:22 +0200

    Refactor: cambiar los for i por foreach para recorrer los jugadores

--- src/main/java/com/escuelait/views/commands/ScoreboardView.java
+++ src/main/java/com/escuelait/views/commands/ScoreboardView.java
@@ -1,5 +1,7 @@
 package com.escuelait.views.commands;
 
+import java.util.HashMap;
+
 import com.escuelait.models.Game;
 import com.escuelait.models.Match;
 import com.escuelait.models.Player;
@@ -19,22 +21,22 @@ class ScoreboardView {
   void write() {
     this.console.writeln("id:" + this.match.getId());
     this.console.writeln("date:" + new DateFormatter().format(this.match.getCreationDate()));
-    for (int i = 0; i < this.match.getPlayers().size(); i++) {
-      this.console.writeln(this.getPlayerPoints(i));
+    for (Player player : this.match.getPlayers()) {
+      this.console.writeln(this.getPlayerPoints(player));
     }
     this.writeSpecialPoints();
   }
 
-  private String getPlayerPoints(int i) {
-    return this.getServiceMark(i) + " " + this.getPlayer(i).name()
-        + ": " + this.getGamePoints(i) + this.getPlayerSetGames(i);
+  private String getPlayerPoints(Player player) {
+    return this.getServiceMark(player) + " " + player.name()
+        + ": " + this.getGamePoints(player) + this.getPlayerSetGames(player);
   }
 
-  private String getServiceMark(int i) {
+  private String getServiceMark(Player player) {
     if (this.match.isFinished()) {
       return "";
     }
-    if (this.match.isServing(this.getPlayer(i))) {
+    if (this.match.isServing(player)) {
       if (this.match.isLackService()) {
         return "+";
       } else {
@@ -44,44 +46,41 @@ class ScoreboardView {
     return " ";
   }
 
-  private String getPlayerSetGames(int i) {
-    String result = "";
-    int[][] setGames = this.getSetGames();
-    for (int j = 0; j < setGames[i].length; j++) {
-      boolean isZeroZero = setGames[0][j] == 0 && setGames[1][j] == 0;
-      result += " " + (isZeroZero ? "-" : setGames[i][j]);
-    }
-    return result;
-  }
-
-  private int[][] getSetGames() {
-    int[][] setGames = new int[this.match.getPlayers().size()][this.match.getNumberOfSets()];
-    for (int i = 0; i < setGames.length; i++) {
-      for (int j = 0; j < this.match.getSetGames(this.getPlayer(i)).size(); j++) {
-        setGames[i][j] = this.match.getSetGames(this.getPlayer(i)).get(j);
-      }
-    }
-    return setGames;
-  }
-
-  private Player getPlayer(int i) {
-    return this.match.getPlayers().get(i);
-  }
-
-  private String getGamePoints(int i) {
+  private String getGamePoints(Player player) {
     if (this.match.isGameFinished()) {
       return "0";
     }
     if (this.match.isTieBreak()) {
-      return this.match.getGamePoints(this.getPlayer(i)) + "";
+      return this.match.getGamePoints(player) + "";
     }
-    if (this.match.getGamePoints(this.getPlayer(i)) < Game.MIN_POINTS_TO_WIN) {
-      return new String[] { "0", "15", "30", "40" }[this.match.getGamePoints(this.getPlayer(i))];
+    if (this.match.getGamePoints(player) < Game.MIN_POINTS_TO_WIN) {
+      return new String[] { "0", "15", "30", "40" }[this.match.getGamePoints(player)];
     }
-    if (this.match.getGamePoints(this.getPlayer(i)) > this.match.getGamePoints(this.getPlayer((i + 1) % 2))) {
+    if (this.match.getGamePoints(player) > this.match.getGamePoints(this.match.getOther(player))) {
       return "AD";
     }
     return "40";
+  }
+
+  private String getPlayerSetGames(Player player) {
+    String result = "";
+    HashMap<Player, int[]> setGames = this.getSetGames();
+    for (int j = 0; j < this.match.getNumberOfSets(); j++) {
+      boolean isZeroZero = setGames.get(player)[j] == 0 && setGames.get(this.match.getOther(player))[j] == 0;
+      result += " " + (isZeroZero ? "-" : setGames.get(player)[j]);
+    }
+    return result;
+  }
+
+  private HashMap<Player, int[]> getSetGames() {
+    HashMap<Player, int[]> setGames = new HashMap<>();
+    for (Player player : this.match.getPlayers()) {
+      setGames.put(player, new int[this.match.getNumberOfSets()]);
+      for (int j = 0; j < this.match.getSetGames(player).size(); j++) {
+        setGames.get(player)[j] = this.match.getSetGames(player).get(j);
+      }
+    }
+    return setGames;
   }
 
   private void writeSpecialPoints() {
